13d538d4390fb7b99263c2724be2c50b1eb2ebaf
CalcPrime465 <zacharyjzoll1@gmail.com>
2026-07-07 15:00:23 +0200

    player movement
    added left and right movement for player
    added bounds

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    public InputAction moveAction;
+
+    private float speed = 15;
+    private float xRange = 12;
+    private Vector2 moveInput;
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        KeepInBounds();
+    }
+
+    void MovePlayer()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+
+        transform.Translate(Vector3.right * moveInput.x * Time.deltaTime * speed);
+    }
+
+    void KeepInBounds()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
