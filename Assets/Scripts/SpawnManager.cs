aec9e90122e4c11b0a7bd91518ab40e813a73647
CalcPrime465 <zacharyjzoll1@gmail.com>
2026-07-13 19:25:31 +0200

    SpawnManager refactor
    refactored SpawnManager to replace hardcoded values with variables

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,28 +5,32 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
 
+    private float initialSpawnRate = 2;
+    private float maxSpawnRate = 0.25f;
+    private float spawnRateIncrease = 1.01f;
+    private float spawnRangeX = 12;
+    private float spawnPosY = 8;
+    private float spawnPosZ = -4;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(EnemySpawnRoutine(2));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        StartCoroutine(EnemySpawnRoutine(initialSpawnRate));
     }
 
     void SpawnEnemy()
     {
-        Vector3 spawn = new Vector3(Random.Range(-12, 12), 8, -4);
-        Instantiate(enemyPrefab, spawn, enemyPrefab.transform.rotation);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), spawnPosY, spawnPosZ);
+        Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
     }
 
     IEnumerator EnemySpawnRoutine(float spawnRate)
     {
         yield return new WaitForSeconds(spawnRate);
-        spawnRate /= 1.01f;
+        if (spawnRate > maxSpawnRate)
+        {
+            spawnRate /= spawnRateIncrease;
+        }
         SpawnEnemy();
         StartCoroutine(EnemySpawnRoutine(spawnRate));
     }
